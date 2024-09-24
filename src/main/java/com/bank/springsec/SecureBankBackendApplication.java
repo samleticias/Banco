d85868ae47449c7af713344eaf747edd0e364fcf
samleticias <sammyaleticia09@gmail.com>
2024-09-24 19:31:50 -0300

    first commit

---
+++ src/main/java/com/bank/springsec/SecureBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bank.springsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureBankBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/springsec/SecureBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.springsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
