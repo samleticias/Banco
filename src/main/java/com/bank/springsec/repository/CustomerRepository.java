b269f73e855d98579e5ec4078b3e4c5dfbc12264
samleticias <sammyaleticia09@gmail.com>
2024-09-27 00:05:47 -0300

    feat: adiciona repositório e classe modelo de Cliente

---
+++ src/main/java/com/bank/springsec/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.bank.springsec.repository;
+
+import com.bank.springsec.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+
+}
